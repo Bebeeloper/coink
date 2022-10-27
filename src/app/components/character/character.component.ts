a8677a2d33201a578295dc567c189170688d44ba
Kevind <profesionaldigital@impormedical.com.co>
2022-10-27 16:14:34 -0500

    Sort system

--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CharactersService } from 'src/app/services/characters.service';
-import { Characters } from 'src/app/models/character.model';
+import { Character } from 'src/app/models/character.model';
 
 @Component({
   selector: 'app-character',
@@ -10,6 +10,9 @@ import { Characters } from 'src/app/models/character.model';
 export class CharacterComponent implements OnInit {
 
   charactersArray: any = [];
+  charactersFilter: any = [];
+  sortName: boolean = false;
+  sortType: boolean = false;
 
   constructor(
     private charactersService: CharactersService
@@ -19,9 +22,36 @@ export class CharacterComponent implements OnInit {
     this.charactersService.getAllCharacters()
     .subscribe((data: any) => {
       for (let index = 0; index < data.results.length; index++) {
-        this.charactersArray.push(data.results[index]);
+        this.charactersArray.push({
+            name: data.results[index].name,
+            species: data.results[index].species
+          }
+        );
       }
+      console.log('array: ' + this.charactersArray[5].species);
     });
+  }
+
+  sortByName(){
+    console.log(this.sortName);
+    if (this.sortName == false) {
+      this.charactersArray.sort((a: any, b: any) => a.name.localeCompare(b.name));
+      this.sortName = !this.sortName;
+    }else{
+      this.charactersArray.sort().reverse();
+      this.sortName = !this.sortName;
+    }
+  }
+
+  sortByType(){
+    console.log(this.sortType);
+    if (this.sortType == false) {
+      this.charactersArray.sort((a: any, b: any) => a.species.localeCompare(b.species));
+      this.sortType = !this.sortType;
+    }else{
+      this.charactersArray.sort().reverse();
+      this.sortType = !this.sortType;
+    }
   }
 
 }
--- src/app/models/character.model.ts
+++ src/app/models/character.model.ts
@@ -1,26 +1,31 @@
-export interface Characters {
-  info:    Info;
-  results: Result[];
-}
+// export interface Characters {
+//   // info:    Info;
+//   results: Result[];
+// }
 
-interface Info {
-  count: number;
-  pages: number;
-  next:  string;
-  prev:  null;
-}
+// // interface Info {
+// //   count: number;
+// //   pages: number;
+// //   next:  string;
+// //   prev:  null;
+// // }
 
-interface Result {
-  id:       number;
-  name:     string;
-  status:   string;
-  species:  string;
-  type:     string;
-  gender:   string;
-  origin:   Location;
-  location: Location;
-  image:    string;
-  episode:  string[];
-  url:      string;
-  created:  Date;
+// interface Result {
+//   id:       number;
+//   name:     string;
+//   // status:   string;
+//   species:  string;
+//   // type:     string;
+//   // gender:   string;
+//   // origin:   Location;
+//   // location: Location;
+//   // image:    string;
+//   // episode:  string[];
+//   // url:      string;
+//   // created:  Date;
+// }
+
+export interface Character {
+  name: string,
+  species: string
 }
