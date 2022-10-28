51251fd1794a1b37c48e22e06c4995a4b8f7c0c0
bebeloper <kevind171819@gmail.com>
2022-10-27 22:12:05 -0500

    filter fixed

--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -38,9 +38,11 @@ export class CharacterComponent implements OnInit {
     console.log(this.sortName);
     if (this.sortName == false) {
       this.charactersArray.sort((a: any, b: any) => a.name.localeCompare(b.name));
+      this.filterCharactersList.sort((a: any, b: any) => a.name.localeCompare(b.name));
       this.sortName = !this.sortName;
     }else{
       this.charactersArray.sort().reverse();
+      this.filterCharactersList.sort().reverse();
       this.sortName = !this.sortName;
     }
   }
@@ -49,15 +51,17 @@ export class CharacterComponent implements OnInit {
     console.log(this.sortType);
     if (this.sortType == false) {
       this.charactersArray.sort((a: any, b: any) => a.species.localeCompare(b.species));
+      this.filterCharactersList.sort((a: any, b: any) => a.species.localeCompare(b.species));
       this.sortType = !this.sortType;
     }else{
       this.charactersArray.sort().reverse();
+      this.filterCharactersList.sort().reverse();
       this.sortType = !this.sortType;
     }
   }
 
   filters(){
-    const filterArray = this.charactersArray.filter(character => character.name.includes(this.filterName)); //after item is a compare statement
+    const filterArray = this.charactersArray.filter(character => character.name.toLowerCase().includes(this.filterName.toLowerCase())); //after item is a compare statement
 
     if (filterArray) {
       this.filterCharactersList = filterArray;
