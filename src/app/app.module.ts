4394d76456eae1e066bbd68b3ecd6157923d6949
kevind <kevind171819@gmail.com>
2022-10-30 12:07:20 -0500

    table-main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharacterComponent } from './components/character/character.component';
+import { MainTableComponent } from './components/main-table/main-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharacterComponent
+    CharacterComponent,
+    MainTableComponent
   ],
   imports: [
     BrowserModule,
